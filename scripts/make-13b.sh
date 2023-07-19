e1eae41aaa8b0c44048a8c8fba7d3af72c3bb32d
soulteary <soulteary@gmail.com>
2023-07-19 23:49:16 +0800

    fix: build scripts

--- scripts/make-13b.sh
+++ scripts/make-13b.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t soulteary/llama2:base .
+docker build -t soulteary/llama2:base . -f docker/Dockerfile.base
 
 docker build -t soulteary/llama2:7b . -f docker/Dockerfile.7b
