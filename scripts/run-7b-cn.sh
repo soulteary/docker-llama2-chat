efe0455d7f2215bf60c673ac495a0fe2b46f4422
soulteary <soulteary@gmail.com>
2023-07-21 15:03:38 +0800

    fix: namespace

--- scripts/run-7b-cn.sh
+++ scripts/run-7b-cn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 --rm -it -v `pwd`/meta-llama:/app/meta-llama -p 7860:7860 soulteary/llama2:7b-cn
+docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 --rm -it -v `pwd`/LinkSoul:/app/LinkSoul -p 7860:7860 soulteary/llama2:7b-cn
